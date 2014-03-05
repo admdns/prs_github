9c91fb26ee41569f9a4b5fc817f45fe52fcd7c45
ce <ce@v>
2014-03-06 01:25:13 +0200

    initialize

---
+++ util/style.js
@@ -0,0 +1,49 @@
+function mouseOver(x)
+{
+switch (x)
+{
+    case -1:
+        document.mm.src ="./image/menu0b.gif";
+        break;
+    case 0:
+        document.m0.src ="./image/menu1b.gif";
+        break;
+    case 1:
+        document.m1.src ="./image/menu2b.gif";
+        break;
+    case 2:
+        document.m2.src ="./image/menu3b.gif";
+        break;
+    case 3:
+        document.m3.src ="./image/menu4b.gif";
+        break;
+    case 4:
+        document.m4.src ="./image/menu5b.gif";
+        break;
+    
+}
+}
+function mouseOut(x)
+{
+switch (x)
+{
+    case -1:
+        document.mm.src ="./image/menu0a.gif";
+        break;
+    case 0:
+        document.m0.src ="./image/menu1a.gif";
+        break;
+    case 1:
+        document.m1.src ="./image/menu2a.gif";
+        break;
+    case 2:
+        document.m2.src ="./image/menu3a.gif";
+        break;
+    case 3:
+        document.m3.src ="./image/menu4a.gif";
+        break;
+    case 4:
+        document.m4.src ="./image/menu5a.gif";
+        break;    
+}
+}
